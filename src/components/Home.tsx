eb5cdead8618906059674993071a03e1ca8aab0e
Kinjal <kinjalhirapara25@gmail.com>
2025-12-02 12:45:08 +1300

    updated file in public folder.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -55,7 +55,7 @@ const Home: React.FC = () => {
           >
             <a
               href="/Kinjal_Front-End_Developer.pdf"
-              download="Kinjal_Front-End_Developer"
+              download
               target="_blank"
               className="inline-block px-6 py-2 rounded border-2 border-primary bg-transparent text-primary font-semibold transition  hover:shadow-[3px_3px_0_0_var(--color-primary)] hover:-translate-x-1 hover:-translate-y-1"
             >
