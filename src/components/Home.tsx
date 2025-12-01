72b7d779d8aa0e9e33d34f86bbf8c4200cc6f0af
Kinjal <kinjalhirapara25@gmail.com>
2025-12-02 12:28:51 +1300

    updated resume link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -55,7 +55,7 @@ const Home: React.FC = () => {
           >
             <a
               href="../assets/Kinjal_Front-End_Developer.pdf"
-              download="Kinjal_Front-End_Developer"
+              download="Kinjal_Front-End_Developer.pdf"
               target="_blank"
               className="inline-block px-6 py-2 rounded border-2 border-primary bg-transparent text-primary font-semibold transition  hover:shadow-[3px_3px_0_0_var(--color-primary)] hover:-translate-x-1 hover:-translate-y-1"
             >
